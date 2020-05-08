771f872716cd57abd2f2d3c5615caed4e8d05b04
Xuezhi Yan <2000chrisyan@gmail.com>
2020-05-08 16:44:12 -0300

    hitbox reader

--- js/Room.js
+++ js/Room.js
@@ -10,7 +10,18 @@ class Room {
 
     read_from(name){
       var contents = fs.readFileSync(path.join("maps", name + ".hitbox"), 'utf8');
-      console.log(contents.split("\r\n"));
+      contents = contents.split("\r\n");
+      for(var line in contents){
+        if(contents[line]){
+          this.hitboxes.push([]);
+          contents[line] = contents[line].split(",");
+          for(var tile in contents[line]){
+            if(contents[line][tile]){
+              this.hitboxes[line].push(contents[line][tile]);
+            }
+          }
+        }
+      }
     }
   }
 
